1c7183dc92ac0d02324f54e9b8e2b13b9ab93874
unknown <senior.dev0425@outlook.com>
2019-10-30 08:46:50 -0700

    App.js revise

--- App.js
+++ App.js
@@ -5,8 +5,6 @@ import AppWithNavigationState from './app/navigators/AppNavigator' ;
 import AppReducer from './app/reducers/index';
 import {middleware} from './app/utils/redux'
 
-
-
 export default class App extends Component{
   store = createStore(AppReducer, applyMiddleware(middleware));  
   render(){
